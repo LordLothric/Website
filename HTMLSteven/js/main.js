be45f7953d75dca4aaacf4dc45921e878d39cb85
Michael Verkoelen <Darkvader_9200@hotmail.com>
2018-11-16 14:28:16 +0100

    Add files via upload

--- HTMLSteven/js/main.js
+++ HTMLSteven/js/main.js
@@ -1,19 +1,42 @@
- $(document).ready(function() {
         var playing = false;
         var obj = document.createElement("audio");
-        obj.src="../audio/Doot.mp3";
-        obj.volume=0.10;
-        obj.autoPlay=false;
-        obj.preLoad=true;       
- 
-     
-     $('.doot').click(function() {
-  if (playing == false) {
-      obj.play();
-      playing = true;
-  } else {
-      obj.pause();
-      playing = false;
-  }
-});  
-    });
+        var obj2 = document.createElement("audio");
+        var obj3 = document.createElement("audio");
+        obj3.src = "../audio/Spooky.mp3";
+        obj2.src = "../audio/pete.mp3";
+        obj.src = "../audio/Doot.mp3";
+        obj.volume = 0.10;
+        obj.autoPlay = false;
+        obj.preLoad = true;
+
+
+        $('.doot').click(function () {
+            if (playing == false) {
+                obj.play();
+                playing = true;
+            } else {
+                obj.pause();
+                playing = false;
+            }
+        });
+
+        $('#keepo').click(function () {
+            if (playing == false) {
+                obj2.play();
+                playing = true;
+            } else {
+                obj2.pause();
+                playing = false;
+            }
+        });
+
+
+        $('.Spooky').click(function () {
+            if (playing == false) {
+                obj3.play();
+                playing = true;
+            } else {
+                obj3.pause();
+                playing = false;
+            }
+        });
